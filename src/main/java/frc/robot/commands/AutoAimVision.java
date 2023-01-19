c8aaf6a786a9e980e1efb7f23ec4d8d2a391e64a
John Burnham <j8axolotl@gmail.com>
2023-01-18 18:59:05 -0800

    AutoAimVision progress

--- src/main/java/frc/robot/commands/AutoAimVision.java
+++ src/main/java/frc/robot/commands/AutoAimVision.java
@@ -14,23 +14,41 @@ public class AutoAimVision extends PIDCommand {
     private double              kToleranceDeg = .5;
     private PhotonCamera        phCamera;
     private boolean             hasTarget, targetLocked;
+    private var result
 
     public AutoAimVision(PhotonCamera phCamera, ){
-        var result = phCamera.getLatestResult();
         
-        if(result.hasTarget){
-            double range =
-                            PhotonUtils.calculateDistanceToTargetMeters(
-                                    CAMERA_HEIGHT_METERS,
-                                    TARGET_HEIGHT_METERS,
-                                    CAMERA_PITCH_RADIANS,
-                                    Units.degreesToRadians(result.getBestTarget().getPitch()));
+        while(){
+            result = phCamera.getLatestResult();
+            
+            translation = PhotonUtils.estimateCameraToTargetTranslation(
+                distanceMeters, Rotation2d.fromDegrees(-target.getYaw()));
+            
+                if(result.hasTarget){
+                double range =
+                                PhotonUtils.calculateDistanceToTargetMeters(
+                                        CAMERA_HEIGHT_METERS,
+                                        TARGET_HEIGHT_METERS,
+                                        CAMERA_PITCH_RADIANS,
+                                        Units.degreesToRadians(result.getBestTarget().getPitch()));
 
-            //figure out the y traslation
-            //calc distance based on x and y
-            //find need yaw, based on closest apirl tag
-            //use swerve drive method to translate
-            //find what data PipelineResult provides
+                //figure out the y traslation
+                //calc distance based on x and y
+                estimateCameraToTargetTranslation()
+                //find need yaw, based on closest apirl tag
+                //use swerve drive method to translate
+                //find what data PipelineResult provides
+                
+                /*use:
+                    estimateCameraToTargetTranslation(estimateFieldToRobotAprilTag(), )
+                    Translation2d translation = PhotonUtils.estimateCameraToTargetTranslation(
+                    distanceMeters, Rotation2d.fromDegrees(-target.getYaw()));
+                 */
+            
+                /* methods to remember:
+                    getTargets
+                */
+            }
         }
         
     }
