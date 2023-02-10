727652b065518552892eff9c2436389c7f8dc236
John Burnham <j8axolotl@gmail.com>
2023-02-09 18:50:29 -0800

    even more progress on arm

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,12 +1,23 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 public class Arm {
 
     private Pose2d          armPose;
+
     private double          radius;
-    private  double         radians;
+    private double          radians;
+    private double          actionTime;
+    private double          power2;
+    private double          radianPerSecond;
+
+    //channel tbd
+    private PWMSparkMax     armMotor1 = new PWMSparkMax(0);
+
+    //channel tbd
+    private PWMSparkMax     armMotor2 = new PWMSparkMax(0);
 
     public Arm(){
 
@@ -21,15 +32,15 @@ public class Arm {
     }
 
     public void setArmPose(double power, Pose2d targetPose){
-        
+        double radianPerSecond = 0.0;
         radius = targetPose.getX()/Math.acos(targetPose.getX());
         radians = Math.asin(targetPose.getY()/radius);
-        actionTime = 0.5/radius
-        2ndPower = radius/()
+        actionTime = 0.5/radius;
+        power2 = radius/(radianPerSecond);
 
 
-        armMotor1.setPower(0.5);
-        armMotor2.setPower(2ndPower);
+        armMotor1.set(0.5);
+        armMotor2.set(power2);
         sleep(actionTime);
 
         //xDist = radius * Math.cos(radians);
